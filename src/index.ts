2a596edb929041d0d2a81d12858e7dd113618487
Vörös Vilmos (Admin) <admin_vorosv@uni-bge.hu>
2021-01-19 19:22:46 +0100

    oop-basics

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,68 @@
 import './style.scss';
 
-class User {
-    name: string = '';            // a string az typescript, megadjuk a typusát         // ide sem írhatok számot, mert stringet vár lent
-    emai: string = '';
-    age: number = 18;
+class Vehicle {
+    name: string;       // így van üresen hagyva
+    /** @var {string} type -can be water, ground, air  */           // így kommentelek szebben
+    type: string;                                                   // lehet vizi, légi és szárazföldi
 
-    constructor(name: string /* = 'Jenő' */) {
+    /* constructor(name: string) {              // jó ez üres konstruktorral is amit a ts legenerál
         this.name = name;
-    }
+    } */
 
-    //  hello(): number {               // ez hibás neki, mert a stringet nem lehet a számhoz rendeltetni, mert számot vár.
-    hello(): string {               // itt megtudjuk jelölni a típust, ezért nevezzük typescriptnek
-        return `Szia a nevem ${this.name}.`;
+    move(from: number, to: number): string {                            // ilyenkor string típusú lesz a metódus ezért a returnjébe egy string kell; Van void is, akkor nincs visszatérési érték
+        return `This vehicle is moving from ${from} to ${to}.`;
     };
+};
+
+class GroundVehicle extends Vehicle {           // az extends kulcsszóval örökli a GroundVehicle a Vehicle tulajdonságait
+    type: string = 'ground';
+    wheels: number = 0;
+
+    constructor(name: string, wheels: number) {
+        super();                                // az ős konstruktorát hívja meg a super() 
+        this.name = name;                       // ha az ősben van konstruktor, akkor ez ide nem kell!!!
+        this.wheels = wheels;
+    }
 }
 
-const user1 = new User('Juli');
-const user2 = new User('Laci');
-//user2.name = 'jenő';
-const user3 = new User('Pistike');
+class Lorry extends GroundVehicle {
+    capacity: number = 0;
+    constructor(name: string, wheels: number, capacity: number) {
+        super(name, wheels);
+        this.capacity = capacity;
+    }
+}
 
-console.log(user3.hello());
-console.log(user3.age, user3.name);
+const volvoTruck = new Lorry('GlobeTrotter', 12, 24000);
+
+console.log(volvoTruck.move(10, 50));
+
+
+// class User {
+//     name: string = '';            // a string az typescript, megadjuk a typusát         // ide sem írhatok számot, mert stringet vár lent
+//     emai: string = '';
+//     age: number = 18;
+
+//     constructor(name: string /* = 'Jenő' */) {
+//         this.name = name;
+//     }
+
+//     //  hello(): number {               // ez hibás neki, mert a stringet nem lehet a számhoz rendeltetni, mert számot vár.
+//     hello(): string {               // itt megtudjuk jelölni a típust, ezért nevezzük typescriptnek
+//         return `Szia a nevem ${this.name}.`;
+//     };
+// }
+
+// const user1 = new User('Juli');
+// const user2 = new User('Laci');
+// //user2.name = 'jenő';
+// const user3 = new User('Pistike');
+
+// console.log(user3.hello());
+// console.log(user3.age, user3.name);
+
+
+
 
 // const user = {
 //     name: 'Juli',
